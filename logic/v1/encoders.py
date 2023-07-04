830b1467691c30a88f79a522298e060776d40d81
pooriataj <pouriyatajmehrabi1381@gmail.com>
2023-07-05 00:16:19 +0330

    Add multiple new files Add new migrations Add Pipefile Add encoders as different namespace Complete token authentication system and login and logout Fix changes to coerce with the navigation direction change in requirements and package

---
+++ logic/v1/encoders.py
@@ -0,0 +1,17 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from django.forms import model_to_dict
+from django.db.models import Model   
+
+class ExtendedEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, Model):
+            oo = model_to_dict(o)
+            for field in o._meta.fields:
+                val = getattr(o, field.name)
+                if isinstance(val, Model):
+                    oo[field.name] = self.default(val)
+            return oo
+        elif isinstance(o, list):
+            return [self.default(item) for item in o]
+        return super().default(o)
+    
